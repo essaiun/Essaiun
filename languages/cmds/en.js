d27d2b505e52618366ec333fb253ecc436cbff6f
AnhDerrick <anhderrick4@gmail.com>
2024-06-24 17:01:51 +0000

    Update en.js

--- languages/cmds/en.js
+++ languages/cmds/en.js
@@ -342,8 +342,8 @@ module.exports = {
 		}
 	},
 	help: {
-		description: "View command usage",
-		guide: "{pn} [empty | <page number> | <command name>]",
+		description: "Voir les cmds et leur fonction et leur mode d'utilisation.",
+		guide: "{pn} : pour voir les commandes. \n {pn} <nombre de la page> : pour voir les cmds qui sont sur la page précisée. \n {pn} <nom de la cmd> : pour voir le fonctionnement et l'utilité de la cmd mentionnée.",
 		text: {
 			help: "╭─────────────⭓\n%1\n├─────⭔\n│ Page [ %2/%3 ]\n│ Currently, the bot has %4 commands that can be used\n│ » Type %5help <page> to view the command list\n│ » Type %5help to view the details of how to use that command\n├────────⭔\n│ %6\n╰─────────────⭓",
 			help2: "%1├───────⭔\n│ » Currently, the bot has %2 commands that can be used\n│ » Type %3help <command name> to view the details of how to use that command\n│ %4\n╰─────────────⭓",
