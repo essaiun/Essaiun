4f526cae74cef7bc71f088c2898d08343c065731
AnhDerrick <anhderrick4@gmail.com>
2024-06-26 18:07:46 +0000

    Update en.js

--- languages/events/en.js
+++ languages/events/en.js
@@ -9,30 +9,30 @@ module.exports = {
 	},
 	leave: {
 		text: {
-			session1: "morning",
-			session2: "noon",
-			session3: "afternoon",
-			session4: "evening",
-			leaveType1: "left the group",
-			leaveType2: "was kicked from the group"
+			session1: "",
+			session2: "",
+			session3: "",
+			session4: "",
+			leaveType1: "𝐚 𝐪𝐮𝐢𝐭𝐭𝐞́ 𝐥𝐞 𝐆𝐫𝐨𝐮𝐩𝐞.",
+			leaveType2: "𝐚 𝐞́𝐭𝐞́ 𝐄́𝐣𝐞𝐜𝐭𝐞́ 𝐝𝐮 𝐠𝐫𝐨𝐮𝐩𝐞."
 		}
 	},
 	logsbot: {
 		text: {
-			title: "====== Bot logs ======",
-			added: "\n✅\nEvent: bot has been added to a new group\n- Added by: %1",
-			kicked: "\n❌\nEvent: bot has been kicked\n- Kicked by: %1",
-			footer: "\n- User ID: %1\n- Group: %2\n- Group ID: %3\n- Time: %4"
+			title: "====== 𝐋𝐄𝐒 𝐍𝐎𝐔𝐕𝐄𝐋𝐋𝐄𝐒 ======",
+			added: "\n✅\n 𝐃𝐔 𝐍𝐎𝐔𝐕𝐄𝐀𝐔: 𝑱'𝒂𝒊 𝒆́𝒕𝒆́ 𝑨𝒋𝒐𝒖𝒕𝒆́ 𝒂̀ 𝒖𝒏 𝑮𝒓𝒐𝒖𝒑𝒆. \n- 𝑷𝒂𝒓: %1",
+			kicked: "\n❌\n𝐃𝐔𝐑𝐄 𝐍𝐎𝐔𝐕𝐄𝐋𝐄: 𝑱'𝒂𝒊 𝒆́𝒕𝒆́ 𝑺𝒖𝒑𝒑𝒓𝒊𝒎𝒆́ 𝒅'𝒖𝒏 𝑮𝒓𝒐𝒖𝒑𝒆. \n- 𝑷𝒂𝒓: %1",
+			footer: "\n- 𝐔𝐈𝐃 𝐃𝐄 𝐋'𝐔𝐓𝐈𝐋𝐈𝐒𝐀𝐓𝐄𝐔𝐑𝐒: %1\n- 𝐆𝐑𝐎𝐔𝐏𝐄: %2\n- 𝐋𝐄 𝐓𝐈𝐃 𝐃𝐔 𝐆𝐑𝐎𝐔𝐏𝐄: %3\n- 𝐋'𝐇𝐞𝐮𝐫𝐞: %4"
 		}
 	},
 	onEvent: {},
 	welcome: {
 		text: {
-			session1: "morning",
-			session2: "noon",
-			session3: "afternoon",
-			session4: "evening",
-			welcomeMessage: "Thank you for inviting me to the group!\nBot prefix: %1\nTo view the list of commands, please enter: %1help",
+			session1: "",
+			session2: "",
+			session3: "",
+			session4: "",
+			welcomeMessage: "𝑺𝒂𝒍𝒖𝒕.. ✌️ 𝑴𝒐𝒊 𝑱𝒆 𝑽𝒊𝒆𝒏𝒔 𝒅𝒆 𝒄𝒉𝒆𝒛 𝒍𝒆𝒔 𝑺𝒂𝒊𝒅𝒚𝒍. 𝑱'𝑬𝒔𝒑𝒆̀𝒓𝒆 𝒒𝒖'𝑶𝒏 𝑷𝒂𝒔𝒔𝒆𝒓𝒂 𝒅𝒆 𝑩𝒐𝒏𝒔 𝑴𝒐𝒎𝒆𝒏𝒕𝒔 𝑬𝒏𝒔𝒆𝒎𝒃𝒍𝒆 🙂 \n \n🅿🆁🅴🅵🅸🆇:  ➪  %1",
 			multiple1: "you",
 			multiple2: "you guys"
 		}
